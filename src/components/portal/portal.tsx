3a591c33e5b973593fcbd9d9d90b263841601930
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-06-06 17:37:18 -0500

    refactor: move finding portal container inside of the component

--- src/components/portal/portal.tsx
+++ src/components/portal/portal.tsx
@@ -1,16 +1,16 @@
 import type { FC, ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
-const modalContainer = document.getElementById('modal-container');
-
-if (!modalContainer) {
-  throw new Error('Modal container element not found');
-}
-
 type PortalProps = {
   children: ReactNode;
 };
 
 export const Portal: FC<PortalProps> = ({children}) => {
+  const modalContainer = document.getElementById('modal-container');
+
+  if (!modalContainer) {
+    throw new Error('Modal container element not found');
+  }
+  
   return createPortal(children, modalContainer);
 }
